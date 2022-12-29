6135f3250ffb055cd2e7f1064fdfd3f32d9b2c76
John Whitson <john.whitson@envoi.io>
2022-12-29 22:27:42 +0000

    Added initial dynamic logo logic

--- src/App.js
+++ src/App.js
@@ -11,11 +11,22 @@ import { Timeline, ChannelItem, ProgramItem } from "./components";
 function App() {
   const { isLoading, getEpgProps, getLayoutProps } = useApp();
 
+  let location = window.location.href
+
+  let logoSrc
+  if (location.includes('1111') || location.includes('11eleven')) {
+    logoSrc = 'https://vlive.imgix.net/1111/screening/images/landingpagelogo.png?auto=compress,format'
+  } else if (location.includes('revolt')) {
+    logoSrc = 'https://revolt.imgix.net/screening/images/landingpagelogo.png?auto=compress,format'
+  } else {
+    logoSrc = '"https://envoi-common-resources.imgix.net/saas/envoi/images/landingpagelogo.png?auto=compress,enhance,format"'
+  }
+
   return (
     <div id="main">
       <div style={{ height: "80vh", width: "100%" }}>
         <img
-          src="https://envoi-common-resources.imgix.net/saas/envoi/images/landingpagelogo.png?auto=compress,enhance,format"
+          src={logoSrc}
           alt="logo"
         />
 
